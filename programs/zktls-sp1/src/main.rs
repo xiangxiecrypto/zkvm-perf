c268df83de977916fc1ee13b6a61dc6590633788
Xiang Xie <xiexiangiscas@gmail.com>
2025-04-15 22:21:58 +0800

    fix

--- programs/zktls-sp1/src/main.rs
+++ programs/zktls-sp1/src/main.rs
@@ -7,13 +7,11 @@ sp1_zkvm::entrypoint!(main);
 
 
 pub fn main() {
-    #[cfg(feature = "sp1")]
     let verifying_key: String = sp1_zkvm::io::read();
-    #[cfg(feature = "sp1")]
     let verifying_data: VerifyingDataOpt = sp1_zkvm::io::read();
 
     let _ = verifying_data.verify(&verifying_key).is_ok();
 
-    #[cfg(feature = "sp1")]
-    sp1_zkvm::io::commit(&verifying_key);
+    //sp1_zkvm::io::commit(&verifying_key);
+    //sp1_zkvm::io::commit(&verifying_data.get_records());
 }
